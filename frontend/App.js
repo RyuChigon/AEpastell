ed533cda43c8248629a441f613a8ce6a04b1bc1b
RyuChigon <fbclrhs10@gmail.com>
2021-02-15 23:16:57 +0900

    Add images

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground style={{ width: "100%", height: "100%" }} source={require("./images/MAIN/Main_background1.jpg")} resizeMode="cover" >
+        <Text>Pastell</Text>
+      </ImageBackground>
     </View>
   );
 }
