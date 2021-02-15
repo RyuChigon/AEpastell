da5e12bc4c6da2ea8f077106fbc27f39047486fa
RyuChigon <fbclrhs10@gmail.com>
2021-02-16 02:58:33 +0900

    Initial screen

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Image, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <ImageBackground style={{ width: "100%", height: "100%" }} source={require("./images/MAIN/Main_background1.jpg")} resizeMode="cover" >
-        <Text>Pastell</Text>
+        <Image style={styles.title} source={require("./images/PASTELL/Pastell_logo.png")} resizeMode="contain" />
+        <Image style={styles.ein} source={require("./images/PASTELL/Pastell_logo_ein.png")} resizeMode="contain" />
+        <View style={styles.btn_welcome}>
+          <Button title="Welcome !" />
+        </View>
       </ImageBackground>
     </View>
   );
@@ -15,8 +19,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    width: "100%",
+    height: "20%",
+    marginTop: "5%",
+  },
+  ein: {
+    width: "100%",
+    height: "30%",
+    marginTop: "10%",
+  },
+  btn_welcome: {
+    marginHorizontal: "20%",
   },
 });
