b6551b669ba33c4336c54b546da022a28a4374f0
RyuChigon <fbclrhs10@gmail.com>
2021-03-31 16:43:41 +0900

    change mysql user

--- backend/dbconnection.js
+++ backend/dbconnection.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 const connection = mysql.createPool({
   host: 'localhost',
   port: 3306,
-  user: 'test',
-  password: 'test1234',
+  user: 'aieducation',
+  password: 'AE@pastell123',
   database: 'pastell'
 });
 
